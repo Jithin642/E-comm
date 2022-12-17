4d7baaf10692b156b657e2f111cd91368646963d
REDDY <reddy@APPLEs-MacBook-Pro-2.local>
2022-12-17 23:38:45 +0530

    product page added

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -1,7 +1,58 @@
 import React from 'react'
+import "./Categories.scss"
+import { Link } from 'react-router-dom'
 
 export const Categories = () => {
   return (
-    <div>Categories</div>
+    <div className='categories'>
+        <div className="col">
+            <div className="row">
+                <img src="https://t3.ftcdn.net/jpg/03/20/68/66/360_F_320686681_Ur6vdYQgDC9WiijiVfxlRyQffxOgfeFz.jpg"></img>
+                <button>
+                    <Link className="link" to="products/1">Sale</Link>
+                </button>
+            </div>
+            <div className="row">
+            <img src="https://media.istockphoto.com/photos/shopping-woman-picture-id1199014988?b=1&k=20&m=1199014988&s=612x612&w=0&h=EXfJHx3r9H9d5QDTYctD3YK-6Hwtat1aR3Y_hxP6ADs="></img>
+                <button>
+                    <Link className="link" to="products/1">Women</Link>
+                </button>
+            </div>
+        </div>
+        <div className="col">
+            <div className="row">
+            <img src="https://thumbs.dreamstime.com/b/supermarket-cart-loaded-cardboard-boxes-sales-goods-concept-trade-commerce-online-shopping-high-delivery-order-134531493.jpg"></img>
+                <button>
+                    <Link className="link" to="products/1">New season</Link>
+                </button>
+            </div>
+        </div>
+        <div className="col col-1">
+            <div className="row">
+                <div className="col">
+                    <div className="row">
+                    <img src="https://t3.ftcdn.net/jpg/03/20/68/66/360_F_320686681_Ur6vdYQgDC9WiijiVfxlRyQffxOgfeFz.jpg"></img>
+                <button>
+                    <Link className="link" to="products/1">men</Link>
+                </button>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="row">
+                    <img src="https://media.istockphoto.com/photos/shopping-woman-picture-id1199014988?b=1&k=20&m=1199014988&s=612x612&w=0&h=EXfJHx3r9H9d5QDTYctD3YK-6Hwtat1aR3Y_hxP6ADs="></img>
+                <button>
+                    <Link className="link" to="products/1">Accessories</Link>
+                </button>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+            <img src="https://thumbs.dreamstime.com/b/supermarket-cart-loaded-cardboard-boxes-sales-goods-concept-trade-commerce-online-shopping-high-delivery-order-134531493.jpg"></img>
+                <button>
+                    <Link className="link" to="products/1">shoes</Link>
+                </button>
+            </div>
+        </div>
+    </div>
   )
 }
--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -3,7 +3,32 @@ import "./Products.scss"
 
 const Products=()=>{
     return(
-        <div>Products</div>
+        <div className="products">
+            <div className="left">
+                <div className="filterItem">
+                    <h2>Product Categories</h2>
+                    <div className="inputItem">
+                        <input type="checkbox" id="1" value={1}/>
+                        <label htmlFor="1">Shoes</label>
+                    </div>
+                    <div className="inputItem">
+                        <input type="checkbox" id="2" value={2}/>
+                        <label htmlFor="2">Skrits</label>
+                    </div>
+                    <div className="inputItem">
+                        <input type="checkbox" id="3" value={3}/>
+                        <label htmlFor="3">Coats</label>
+                    </div>
+                </div>
+                <div className="filterItem">
+                    <h2>Filter by price</h2>
+
+                </div>
+                <div className="filterItem">
+                    <h2>Sort by</h2>
+                </div>
+            </div>
+        </div>
     )
 }
 
