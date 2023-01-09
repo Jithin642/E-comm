eefec790efbc38e47b6535be6db42e4bf22bcad0
REDDY <reddy@APPLEs-MacBook-Pro-2.local>
2023-01-09 23:09:37 +0530

    checking

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { useState } from "react";
 import "./Product.scss"
 
 const Product=()=>{
-    return(
-        <div>product</div>
+    const [selectedImg, setSelectedImg]=useState(0)
+
+    const images=["https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQfCbyF7xiclVEDDt2eL1x6Kj4Z-szikEwT93bSD12ktHTQvI-t8rutPFIxhHTWk157RwObBFomhu90MFP36JACAKymRuYfHV91uFNaGSS0T1FsRJcPOmMGpQ&usqp=CAY", "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSm6UC5e_yk94JNJLzItHSWMLaYAZT7d8fXFwygwCo2gzI5MtyVHg6U1N_YCU-GOxxy9GJw1fI1wzCPrMf1kPzdo0Uyk57zZ_kcsUarVWU&usqp=CAE"];
+
+    return (
+        <div className="product">
+            <div className="left">
+                <div className="images">
+                    <img src={images[0]} alt="" onClick={(e)=>setSelectedImg(0)}/>
+                    <img src={images[1]} alt="" onClick={(e)=>setSelectedImg(1)}/>
+                </div>
+                <div className="mainImg">
+                    <img src={images[selectedImg]} alt=""/>
+                </div>
+            </div>
+            <div className="right"></div>
+        </div>
     )
 }
 
--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,32 +1,55 @@
 import React from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import List from "../../components/List/List";
 import "./Products.scss"
 
 const Products=()=>{
+    const catId=parseInt(useParams().id)
+    const [maxPrice,setMaxPrice]=useState(1000)
+    const [sort,setSort]=useState(null)
+
     return(
         <div className="products">
             <div className="left">
                 <div className="filterItem">
                     <h2>Product Categories</h2>
                     <div className="inputItem">
-                        <input type="checkbox" id="1" value={1}/>
-                        <label htmlFor="1">Shoes</label>
+                        <input type="checkbox" id="1" value={1} />
+                        <label htmlFor='1'>Shoes</label>
                     </div>
                     <div className="inputItem">
-                        <input type="checkbox" id="2" value={2}/>
-                        <label htmlFor="2">Skrits</label>
+                        <input type="checkbox" id="2" value={2} />
+                        <label htmlFor='2'>Coats</label>
                     </div>
                     <div className="inputItem">
-                        <input type="checkbox" id="3" value={3}/>
-                        <label htmlFor="3">Coats</label>
+                        <input type="checkbox" id="3" value={3} />
+                        <label htmlFor='3'>Dresses</label>
                     </div>
                 </div>
                 <div className="filterItem">
                     <h2>Filter by price</h2>
-
+                    <div className="inputItem">
+                        <span>0</span>
+                        <input type="range" min={0} max={1000} onChnage={(e)=>setMaxPrice(e.target.value)}/>
+                        <span>{maxPrice}</span>
+                    </div>
                 </div>
                 <div className="filterItem">
                     <h2>Sort by</h2>
+                    <div className="inputItem">
+                        <input type="radio" id="asc" value="asc" name="price" onChange={(e)=>setSort("asc")}/>
+                        <label htmlFor="asc">Price (Lowest first)</label>
+                    </div>
+                    <div className="inputItem">
+                        <input type="radio" id="desc" value="desc" name="price" onChange={(e)=>setSort("desc")} />
+                        <label htmlFor="desc">Price (Highest first)</label>
+                    </div>
                 </div>
+            </div>
+            <div className="right">
+                <img src="https://cdn.pixabay.com/photo/2017/03/13/17/26/ecommerce-2140604__480.jpg" alt="no" />
+                <List catId={catId} maxPrice={maxPrice} sort={sort} /> 
             </div>
         </div>
     )
